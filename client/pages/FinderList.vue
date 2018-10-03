8cb988f05322e7b10e788f56f393fd93bd6b97bf
Willy Li <willy.li@linecorp.com>
2018-10-03 12:55:41 +0800

    Refactor list page

--- client/pages/FinderList.vue
+++ client/pages/FinderList.vue
@@ -1,69 +1,103 @@
 <template>
-  <div id="finderList">
-    <PlaceList />
-  </div>
+  <div class="finderList">
+    <ul>
+      <li v-for="place in places" :key="place.place_id">
+        <Place :placeInfo="place" />
+      </li>
+    </ul>
+    <div class="loading" v-if="isLoading"><img src="../assets/loading.gif" alt=""></div>
+    </div>
 </template>
 
 <script>
-import PlaceList from '@/components/PlaceList'
+import Place from '@/components/Place'
+import { mapState, mapActions } from 'vuex'
 
 export default {
-  name: 'FinderList',
-  components: { PlaceList },
+  name: 'finder-list',
+  components: { Place },
   data () {
     return {
-      scrollTop: 0,
-      bodyHeight: 0,
-      windowHeight: 0,
-      scrollThreshold: 300
+      scrollThreshold: 300,
+      rankby: 'distance',
+      location: '',
+      type: '',
+      scrollElement: {}
     }
   },
-  couputed: {
-    offset () {
-      return this.bodyHeight - this.windowHeight
-    }
+  computed: {
+    ...mapState({
+      places: state => state.places,
+      isLoading: state => state.isLoading
+    })
   },
   methods: {
     scrollHandler () {
-      let location = this.$route.query.location.trim()
-      let type = this.$route.query.types.trim()
-      if (typeof window.pageYOffset !== 'undefined') {
-        this.scrollTop = window.pageYOffset
-      } else if (typeof document.compatMode !== 'undefined' &&
-             document.compatMode !== 'BackCompat') {
-        this.scrollTop = document.documentElement.scrollTop
-      } else if (typeof document.body !== 'undefined') {
-        this.scrollTop = document.body.scrollTop
-      }
-      this.bodyHeight = document.querySelector('#app').clientHeight
-      this.windowHeight = window.innerHeight
-      if (this.bodyHeight - this.windowHeight - this.scrollThreshold <= this.scrollTop) {
+      let scrollTop = this.scrollElement.element[this.scrollElement.attr]
+      let bodyHeight = document.querySelector('#app').clientHeight
+      let windowHeight = window.innerHeight
+      if (bodyHeight - windowHeight - this.scrollThreshold <= scrollTop) {
         window.removeEventListener('scroll', this.scrollHandler)
-        this.$store.dispatch('getList', {
-          location,
-          rankby: 'distance',
-          types: type,
+        this.getList({
+          location: this.location,
+          rankby: this.rankby,
+          types: this.type,
           scrollHandler: this.scrollHandler
         })
       }
-    }
+    },
+    ...mapActions(['getList'])
   },
-  beforeMount () {
-    //  Request nearby places data from google place api
-    let location = this.$route.query.location.trim()
-    let type = this.$route.query.types.trim()
-    let params = {
-      location,
-      rankby: 'distance',
-      types: type
-    }
-    this.$store.commit('setLocation', location)
-    this.$store.dispatch('getList', params)
+  created () {
+    this.location = this.$route.query.location.trim()
+    this.type = this.$route.query.types.trim()
 
+    if (typeof window.pageYOffset !== 'undefined') {
+      this.scrollElement = {element: window, attr: 'pageYOffset'}
+    } else if (typeof document.compatMode !== 'undefined' && document.compatMode !== 'BackCompat') {
+      this.scrollElement = {element: document.documentElement, attr: 'scrollTop'}
+    } else if (typeof document.body !== 'undefined') {
+      this.scrollElement = {element: document.body, attr: 'scrollTop'}
+    }
+
+    this.$store.commit('setLocation', location)
+    this.getList({
+      location: this.location,
+      rankby: this.rankby,
+      types: this.type,
+      scrollHandler: this.scrollHandler
+    })
+  },
+  mounted () {
     window.addEventListener('scroll', this.scrollHandler)
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.finderList {
+  ul {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    background: #eaeaea;
+  }
+  li {
+    background: #ffffff;
+    border: 1px solid #dddddd;
+    margin-top: 20px;
+    &:first-child {
+      margin-top: 0;
+    }
+  }
+}
+.loading {
+  width: 100%;
+  text-align: center;
+  height: 50px;
+  line-height: 50px;
+  img {
+    height: 50px;
+  }
+}
 </style>
